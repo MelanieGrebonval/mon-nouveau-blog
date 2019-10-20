7b79afff6cffd4384b6ceb73585fd6b2494ac263
Melanie <melanie.grebonval@gmail.com>
2019-10-20 15:01:23 +0200

    Modified templates to display posts from database.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+from .models import Post
 
+admin.site.register(Post)
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
